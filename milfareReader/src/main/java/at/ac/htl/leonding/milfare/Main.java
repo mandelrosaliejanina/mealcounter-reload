0fc7430176a8410e7589145a093017445a53f691
tangsandy <sandytang2406@gmail.com>
2022-03-04 10:27:00 +0100

    adjust code to state diagram

--- milfareReader/src/main/java/at/ac/htl/leonding/milfare/Main.java
+++ milfareReader/src/main/java/at/ac/htl/leonding/milfare/Main.java
@@ -2,26 +2,53 @@ package at.ac.htl.leonding.milfare;
 
 import com.fazecast.jSerialComm.SerialPort;
 
+import java.io.IOException;
+
 public class Main {
     static MifareReader reader;
+    enum State {
+        initial,
+        cardFound,
+        cardNotFound,
+        end
+    }
+    static State state = State.initial;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         String id= "";
         findReader();
-        while(true ){
-            reader.open();
-            reader.enquiryCard();
-            reader.close();
+        while(state != State.end) {
 
-            reader.open();
-            while (id != null) {
-                id = reader.anticollision();
-                reader.close();
+            switch (state){
+                case initial:
+                     reader.open();
+                     state = State.cardNotFound;
+                     break;
+                case cardNotFound:
+                    if(id == null){
+                        id = "";
+                    }
+                    if(!id.equals("")){
+                        TagLoader tagLoader = new TagLoader();
+                        tagLoader.sendData("http://152.70.175.53/api/nfccard/create", id);
+                        System.out.println("data is send");
+                        break;
+                    }
+                    id = reader.anticollision();
+                    if(id != null){
+                        state = State.cardFound;
+                    }
+                case cardFound:
+                    if(reader.enquiryCard()){
+                        var cardstate = reader.enquiryCard(); // if card is present enquiryCard is false
+                        state = State.cardNotFound;
+                    }
+                    break;
+                default:
+                    System.out.println("Falsche Zustand");
+                    break;
             }
-            TagLoader tagLoader = new TagLoader();
-            tagLoader.sendData("http://152.70.175.53/api/nfccard/assign-card/" +  id);
         }
-
     }
 
     private static void findReader() {
@@ -42,8 +69,6 @@ public class Main {
         'enquiry_all_cards': '\x03\x02\x01\x05',
                 'anticollision' : '\x02\x03\x05\x00',
                 'select_card' : '\x02\x04\x06"}*/
-
-
 
     public static boolean portIsOpen(SerialPort comPort) {
         var reader = new MifareReader(comPort);
