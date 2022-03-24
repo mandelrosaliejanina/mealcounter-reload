8a3390731627285e4269f105922c5878275b685c
rosaliemandel <r.mandel@students.htl-leonding.ac.at>
2022-03-24 21:19:10 +0100

    update frontend

--- appsrv/src/main/java/at/htl/mealcounter/control/InitBean.java
+++ appsrv/src/main/java/at/htl/mealcounter/control/InitBean.java
@@ -11,6 +11,7 @@ import javax.enterprise.event.Observes;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
+import java.time.LocalDateTime;
 
 @ApplicationScoped
 public class InitBean {
@@ -40,7 +41,19 @@ public class InitBean {
         personRepository.persist(sandy);
         Person kelly = new Person(nfcRepository.findByNfcId("060317f2fb00"),"Kelly","Tran",2012);
         personRepository.persist(kelly);
+        Person franz = new Person(nfcRepository.findByNfcId("0bcb245"),"Franz","Birker",2012);
+        personRepository.persist(franz);
+        Person paula = new Person(nfcRepository.findByNfcId("abncj435"),"Paula","Taler",2012);
+        personRepository.persist(paula);
+        Person susi = new Person(nfcRepository.findByNfcId("235313jn"),"Susi","Maus",2012);
+        personRepository.persist(susi);
 
+        Consumation consumation = new Consumation(paula, LocalDateTime.now().minusDays(1), true);
+        consumationRepository.merge(consumation);
+        Consumation consumation1 = new Consumation(franz, LocalDateTime.now(), true);
+        consumationRepository.merge(consumation1);
+        Consumation consumation2 = new Consumation(susi, LocalDateTime.now(), true);
+        consumationRepository.merge(consumation2);
 
 
     }
--- appsrv/src/main/resources/db/import.sql
+++ appsrv/src/main/resources/db/import.sql
@@ -2,6 +2,9 @@
 insert into m_nfc_card(nfc_id, register_date_time) values ('0603e7e30000',current_date);
 insert into m_nfc_card(nfc_id, register_date_time) values ('060367a5fe00',current_date);
 insert into m_nfc_card(nfc_id, register_date_time) values ('060317f2fb00',current_date);
+insert into m_nfc_card(nfc_id, register_date_time) values ('0bcb245',current_date);
+insert into m_nfc_card(nfc_id, register_date_time) values ('abncj435',current_date);
+insert into m_nfc_card(nfc_id, register_date_time) values ('235313jn',current_date);
 
 
 
